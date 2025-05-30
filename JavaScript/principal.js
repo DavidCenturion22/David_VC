9b56ff37abad86f784987d4026ae0780916ab7f4
DavidCenturion22 <davidverace22@gmail.com>
2025-05-29 23:47:51 -0600

    commit

--- JavaScript/principal.js
+++ JavaScript/principal.js
@@ -1,9 +1,9 @@
 //const saludame=require("./archivo.js");
 //console.log(saludame);
-//console.log((saludame.saludo('Joshua')));
+//console.log((saludame.saludo('David')));
 //const saludame2=require("./archivo.js");
 //console.log((saludame2.saludo2('')));
 
 const {sayHello, sayHelloWorld} = require('./archivo.js');
-console.log(sayHello('Joshua'));
+console.log(sayHello('David'));
 console.log(sayHelloWorld());
